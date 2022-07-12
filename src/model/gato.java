0466cf2379183759be06fc4d81d06762203a34b0
Beatriz Costa <beatrizcostaleal1996@gmail.com>
2022-07-12 13:34:13 -0300

    first commit

---
+++ src/model/gato.java
@@ -0,0 +1,43 @@
+package model;
+
+public class gato {
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+
+    public gato() {
+    }
+
+    public gato(String nome, String cor, Integer idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+}
+
